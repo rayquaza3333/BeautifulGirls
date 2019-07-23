796881bb12cb22acdd271772fcf1e3bbd292c0fe
Pham Hoang <rayquaza3333@gmail.com>
2019-07-24 00:43:22 +0700

    Commit 190723 works

--- 2019.05.08 Django_Jose/2019.05.11_Practical_example/b- Back-End/Repractice/reviewProject/reviewApp/views.py
+++ 2019.05.08 Django_Jose/2019.05.11_Practical_example/b- Back-End/Repractice/reviewProject/reviewApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from forms import UserForm, UserProfileInformationForm
 
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+# index
+
+
+# login
+
+# special
+# logout
+
+# register
